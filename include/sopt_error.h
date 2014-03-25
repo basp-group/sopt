21378867b6e516fef72998adcc3312280d6ea80d
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-03-25 15:18:01 +0000

    inline functions now follow C99 standard

--- include/sopt_error.h
+++ include/sopt_error.h
@@ -5,7 +5,22 @@
 #ifndef SOPT_ERROR
 #define SOPT_ERROR
 
-inline void SOPT_ERROR_GENERIC(char *comment);
-inline void SOPT_ERROR_MEM_ALLOC_CHECK(void *pointer);
+#include <stdio.h>
+#include <stdlib.h>
+
+inline void SOPT_ERROR_GENERIC(char *comment) {
+  printf("ERROR: %s.\n", comment);					
+  printf("ERROR: %s <%s> %s %s %s %d.\n",				
+    "Occurred in function",					
+    __PRETTY_FUNCTION__,						
+    "of file", __FILE__,						
+    "on line", __LINE__);					
+  exit(1);
+}
+
+inline void SOPT_ERROR_MEM_ALLOC_CHECK(void *pointer) {				
+  if(pointer == NULL)
+    SOPT_ERROR_GENERIC("Memory allocation failed");
+}
 
 #endif
