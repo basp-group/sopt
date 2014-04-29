3104cf5e141123f6ee8a40fda5648ee916868e83
Jens H. Nielsen <jens.nielsen@ucl.ac.uk>
2014-04-29 12:14:22 +0100

    Don't use relative links.
    This has issues with homebrew since the share/cmake dir is a symlink
    into the homebrew Cellar

--- cmake_files/export_sopt.cmake
+++ cmake_files/export_sopt.cmake
@@ -17,11 +17,7 @@ configure_File(cmake_files/SoptConfigVersion.in.cmake
 )
 
 # Then for installation tree
-file(RELATIVE_PATH REL_INCLUDE_DIR
-    "${CMAKE_INSTALL_PREFIX}/share/cmake/sopt"
-    "${CMAKE_INSTALL_PREFIX}/include/sopt"
-)
-set(ALL_INCLUDE_DIRS "\${Sopt_CMAKE_DIR}/@REL_INCLUDE_DIR@")
+set(ALL_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/sopt")
 configure_File(cmake_files/SoptConfig.in.cmake
     "${PROJECT_BINARY_DIR}/CMakeFiles/SoptConfig.cmake" @ONLY
 )
