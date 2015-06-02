99f4b2674e35f1f24619b6e45cd2d2f40190bbe4
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-06-02 14:42:24 +0100

    Simplifying unit-testing
    The main for catch is now handled by the build system. This should both increase compilation speed
    and simplify adding new tests.
    
    A main for catch is generated by cmake and compiled into an object library. The object library is
    subsequently re-used by all test executables.

--- cmake_files/catch.cmake
+++ cmake_files/catch.cmake
@@ -8,10 +8,11 @@ if(NOT CATCH_FOUND)
   set(catch_url
       https://raw.githubusercontent.com/philsquared/Catch/develop/single_include/catch.hpp)
   set(catch_file "${EXTERNAL_ROOT}/include/catch.hpp")
+  set(catch_md5 28cf206675d0509d1a92b01543aced8e)
   file(MAKE_DIRECTORY "${EXTERNAL_ROOT}/include")
   file(DOWNLOAD ${catch_url} "${catch_file}")
-  file(SIZE "${catch_file}" size)
-  if(size EQUAL 0)
+  file(MD5 "${catch_file}" actual_md5)
+  if(NOT actual_md5 STREQUAL catch_md5)
     # CMake can't download over https if build lacks ssl. So use wget or curl
     find_package(Wget)
     if(WGET_FOUND)
@@ -20,13 +21,30 @@ if(NOT CATCH_FOUND)
       find_program(CURL_EXECUTABLE curl)
       execute_process(COMMAND ${CURL_EXECUTABLE} -L ${catch_url} -o "${catch_file}")
     endif()
+    file(MD5 "${catch_file}" expected_md5)
+    if(NOT catch_md5 STREQUAL expected_md5)
+      file(REMOVE "${catch_file}")
+      message(FATAL_ERROR "Downloaded catch.hpp, but md5 does not match.")
+    endif()
   endif()
   find_package(Catch REQUIRED)
 endif()
 
+# Function to create a common main
+function(common_catch_main)
+  if(TARGET common_catch_main_object)
+    return()
+  endif()
+  file(WRITE "${CMAKE_BINARY_DIR}/common_catch_main.cc"
+    "#define CATCH_CONFIG_MAIN\n"
+    "#include \"catch.hpp\"\n"
+  )
+  add_library(common_catch_main_object OBJECT "${CMAKE_BINARY_DIR}/common_catch_main.cc")
+endfunction()
+
 # Then adds a function to create a test
 function(add_catch_test testname)
-  cmake_parse_arguments(catch "" "WORKING_DIRECTORY" "LIBRARIES;LABELS" ${ARGN})
+  cmake_parse_arguments(catch "NOMAIN" "WORKING_DIRECTORY" "LIBRARIES;LABELS" ${ARGN})
 
   # Source deduce from testname if possible
   unset(source)
@@ -34,8 +52,18 @@ function(add_catch_test testname)
     set(source ${testname}.cc)
   endif()
 
-  # Construct executable
-  add_executable(test_${testname} ${source} ${catch_UNPARSED_ARGUMENTS})
+
+  # By default, uses a common main function for all, compiled once
+  # We create here
+  if(catch_NOMAIN)
+    add_executable(test_${testname} ${source} ${catch_UNPARSED_ARGUMENTS})
+  else()
+    common_catch_main()
+    # Construct executable
+    add_executable(test_${testname}
+      ${source} $<TARGET_OBJECTS:common_catch_main_object> ${catch_UNPARSED_ARGUMENTS})
+  endif()
+
   if(catch_LIBRARIES)
     target_link_libraries(${testname} ${catch_LIBRARIES})
   endif()
