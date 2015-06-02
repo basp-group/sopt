42187930f9503a935fb97c4365a5b60c091b4340
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-06-02 20:30:39 +0100

    Adding convolve for indirect transform

--- tests/wavelet_convolution.cc
+++ tests/wavelet_convolution.cc
@@ -1,3 +1,4 @@
+#include <random>
 #include "catch.hpp"
 
 #include "wavelets/convolve.impl.cc"
@@ -19,6 +20,9 @@ TEST_CASE("Periodic convolution operator", "[wavelet]") {
     CHECK(periodic_scalar_product(large, small, 4) == 1 * 8 + 2 * 9 + 3 * 4);
     // with wrapping and expression
     CHECK(periodic_scalar_product(large, small.reverse(), 4) == 3 * 8 + 2 * 9 + 1 * 4);
+    // wrapping works with offset as well
+    CHECK(periodic_scalar_product(large, small, 4 + large.size()) == 1 * 8 + 2 * 9 + 3 * 4);
+    CHECK(periodic_scalar_product(large, small, 4 - 3 * large.size()) == 1 * 8 + 2 * 9 + 3 * 4);
 
     // signal smaller than filter
     CHECK(periodic_scalar_product(small, large.head(4), 1) == 4 * 2 + 5 * 3 + 6 * 1 + 7 * 2);
@@ -33,5 +37,38 @@ TEST_CASE("Periodic convolution operator", "[wavelet]") {
     CHECK(result(1) == 3 * 5 + 2 * 6 + 1 * 7);
     CHECK(result(3) == 3 * 7 + 2 * 8 + 1 * 9);
     CHECK(result(4) == 3 * 8 + 2 * 9 + 1 * 4);
+  }
+
+  SECTION("Convolve and sum") {
+    Eigen::Matrix<int, Eigen::Dynamic, 1> result(large.size());
+    Eigen::Matrix<int, Eigen::Dynamic, 1> noOffset(large.size());
+
+    // Check that if high pass is zero, then this is an offseted convolution
+    convolve_sum(result, large, small, large, 0 * small);
+    convolve(noOffset, large, small);
+    CHECK(result(small.size() - 1) == noOffset(0));
+    CHECK(result(0) == noOffset(result.size() - small.size() + 1));
+
+    // Check same for low pass
+    convolve_sum(result, large, 0 * small, large, small);
+    CHECK(result(small.size() - 1) == noOffset(0));
+    CHECK(result(0) == noOffset(result.size() - small.size() + 1));
+
+    // Check symmetry relationships
+    auto const trial = [&small, &large](int a, int b, int c, int d) {
+      Eigen::Matrix<int, Eigen::Dynamic, 1> result(large.size());
+      convolve_sum(result, a * large, b * small, c * large, d * small);
+      return result;
+    };
+
+    // should all be ok as long as arguments sum: (a * b) + (c * d) == (a' * b') + (c' * d')
+    CHECK(trial(0, 1, 3, 1) == trial(0, 1, 1, 3));
+    CHECK(trial(5, 1, 3, 1) == trial(3, 1, 5, 1));
+    CHECK(trial(1, 5, 3, 1) == trial(3, 1, 5, 1));
+    CHECK(trial(1, 3, 5, 1) == trial(3, 1, 5, 1));
+    CHECK(trial(1, 3, 1, 5) == trial(3, 1, 5, 1));
+    CHECK(trial(1, 0, 4, 2) == trial(3, 1, 5, 1));
+    CHECK(trial(1, -1, 1, 1) == trial(0, 1, 0, 1));
+    CHECK(trial(4, -3, 2, 6) == trial(0, 1, 0, 1));
   }
 }
--- wavelets/convolve.impl.cc
+++ wavelets/convolve.impl.cc
@@ -2,21 +2,31 @@
 
 // Function inside anonymouns namespace won't appear in library
 namespace sopt { namespace wavelets { namespace {
-    //! Applies scalar product of the size b
-    //! Elements from a are wrapped around to match b
+    //! \brief Applies scalar product of the size b
+    //! \details In practice, the operation is equivalent to the following
+    //! - `a` can be seen as a periodic vector: `a[i] == a[i % a.size()]`
+    //! - `a'` is the segment `a` = a[offset:offset+b.size()]`
+    //! - the result is the scalar product of `a'` by `b`.
     template<class T0, class T2>
       typename T0::Scalar periodic_scalar_product(
-          Eigen::MatrixBase<T0> const &a, Eigen::MatrixBase<T2> const &b, t_uint offset) {
-        // Simple, just do it
-        if(a.size() > b.size() + offset)
-          return (a.segment(offset, b.size()).transpose() * b);
+          Eigen::MatrixBase<T0> const &a, Eigen::MatrixBase<T2> const &b, t_int offset) {
+        auto const Na = static_cast<t_int>(a.size());
+        auto const Nb = static_cast<t_int>(b.size());
+        // offset in [0, a.size()[
+        offset %= Na;
+        if(offset < 0) offset += Na;
+        // Simple case, just do it
+        if(Na > Nb + offset)
+          return (a.segment(offset, Nb).transpose() * b);
+        // Wrap around, do it, but carefully
         t_real result(0);
-        for(t_uint i(0); i < t_uint(b.size()); ++i)
-          result += a((i+offset) % a.size()) * b(i);
+        for(t_int i(0), j(offset); i < Nb; ++i, ++j)
+          result += a(j % Na) * b(i);
         return result;
       }
 
-    // Convolve low or high pass
+    //! \brief Convolves the signal by the filter
+    //! \details The signal is seen as a periodic vector during convolution
     template<class T0, class T1, class T2>
       void convolve(
           Eigen::MatrixBase<T0> &result,
@@ -24,8 +34,30 @@ namespace sopt { namespace wavelets { namespace {
           Eigen::MatrixBase<T2> const &filter)
     {
       assert(result.size() == signal.size());
-      for(t_uint i(0); i < t_uint(signal.size()); ++i)
+      for(t_int i(0); i < t_int(signal.size()); ++i)
         result(i) = periodic_scalar_product(signal, filter.reverse(), i);
     }
+
+    //! Convolve and sims low and high pass of a signal
+    template<class T0, class T1, class T2, class T3, class T4>
+      void convolve_sum(
+          Eigen::MatrixBase<T0> &result,
+          Eigen::MatrixBase<T1> const &low_pass_signal,
+          Eigen::MatrixBase<T2> const &low_pass,
+          Eigen::MatrixBase<T3> const &high_pass_signal,
+          Eigen::MatrixBase<T4> const &high_pass)
+    {
+      assert(result.size() == low_pass_signal.size());
+      assert(result.size() == high_pass_signal.size());
+      assert(low_pass.size() == high_pass.size());
+
+      auto const loffset = low_pass.size() - 1;
+      auto const hoffset = high_pass.size() - 1;
+      for(t_int i(0); i < static_cast<t_int>(result.size()); ++i)
+        result(i) =
+          periodic_scalar_product(low_pass_signal, low_pass.reverse(), i - loffset)
+          + periodic_scalar_product(high_pass_signal, high_pass.reverse(), i - hoffset);
+    }
+
   }
 }}
