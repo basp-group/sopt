f9f385bee2323338b32aeec05109aedd6c15c2ca
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-06-15 10:33:23 +0100

    Remove unnecessary file

--- cpp/wavelets/wavelets.impl.cc
+++
@@ -1,9 +0,0 @@
-#include <type_traits>
-#include "types.h"
-
-// Holds convolution, up-down sampling, etc.
-#include "innards.impl.cc"
-// Holds direct transform implementation
-#include "direct.impl.cc"
-// Holds indirect transform implementation
-#include "indirect.impl.cc"
