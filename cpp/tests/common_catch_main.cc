c52e149b4404258ef1d25def91d324dd706fea8b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-08-13 17:24:43 +0100

    Links logging and unit-testing, badly
    It is not evident how to suppress output during unit-testing.
    Catch does its own kind of logging, pushing and popping message as it goes through different scopes.
    That approach does not work well with logging.
    
    Closes #24

---
+++ cpp/tests/common_catch_main.cc
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_RUNNER
+
+#include "catch.hpp"
+#include <sopt/logging.h>
+
+int main( int argc, char* const argv[] )
+{
+  Catch::Session session; // There must be exactly once instance
+
+  int returnCode = session.applyCommandLine( argc, argv );
+  if( returnCode != 0 ) // Indicates a command line error
+    return returnCode;
+
+# ifdef SOPT_DO_LOGGING
+#  ifndef SOPT_TEST_DEBUG_LEVEL
+#    define SOPT_TEST_DEBUG_LEVEL debug
+#  endif
+    auto const logger = sopt::logging::initialize();
+    logger->set_level(spdlog::level::SOPT_TEST_DEBUG_LEVEL);
+# endif
+
+  return session.run();
+}
