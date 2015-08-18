fa7b333330ddd957a1ae5734f6cabe47ff232ea5
sinanshi <s.shi@ucl.ac.uk>
2015-08-18 21:53:17 +0100

    remove overloaded function direct/indirect

--- python/wavelets/python.wavelets.h
+++ python/wavelets/python.wavelets.h
@@ -5,6 +5,7 @@
 #include <wavelets.h>
 #include <types.h>
 namespace sopt { namespace pyWavelets{
+  
   template<class T>
     Eigen::Matrix<T,Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
     FromPyMat2EigenMat(T *pyMat, int nrow, int ncol){
@@ -14,8 +15,7 @@ namespace sopt { namespace pyWavelets{
       eigenMat =Eigen::Map<t_pyMatrix>(&pyMat[0],nrow,ncol);
       return(eigenMat);
     }
-
-
+  
   template <class T>
     void direct(T *py_input, T* py_output, std::string name, t_uint level,
         const int nrow, const int ncol){
@@ -25,19 +25,7 @@ namespace sopt { namespace pyWavelets{
       for(int i=0; i<nrow*ncol; ++i)
         py_output[i]=coefficient.data()[i];
     };
-
-  template <class T>
-    void direct(T *py_input, T* py_coefficient, T* py_output, std::string name,\
-        t_uint level, const int nrow, const int ncol){
-      auto const wavelets = sopt::wavelets::factory(name, level);
-      auto eigen_output = FromPyMat2EigenMat(py_output, nrow, ncol);
-      auto eigen_coefficient = FromPyMat2EigenMat(py_coefficient, nrow, ncol);
-      wavelets.direct(eigen_output, eigen_coefficient);
-      for(int i=0; i<nrow*ncol; ++i)
-        py_output[i]=eigen_output.data()[i];
-//      std::cout<<eigen_outputc<std::endl;
-    };
-
+  
   template <class T>
     void indirect(T *py_input, T *py_output, std::string name, t_uint level,\
         const int nrow, const int ncol){
@@ -47,26 +35,5 @@ namespace sopt { namespace pyWavelets{
       for(int i=0; i<nrow*ncol; ++i)
         py_output[i]=coefficient.data()[i];
     };
-  
-  template <class T>
-    void indirect(T *py_input, T* py_coefficient, T* py_output, std::string name,\
-        t_uint level, const int nrow, const int ncol){
-      auto const wavelets = sopt::wavelets::factory(name, level);
-      auto eigen_output = FromPyMat2EigenMat(py_output, nrow, ncol);
-      auto eigen_coefficient = FromPyMat2EigenMat(py_coefficient, nrow, ncol);
-
-      std::cout<<eigen_output<<std::endl;
-
-      wavelets.indirect(eigen_output, eigen_coefficient);
-      for(int i=0; i<nrow*ncol; ++i){
-        py_output[i]=eigen_output.data()[i];
-        //py_coefficient[i]=eigen_coefficient.data()[i];
-      }
-      std::cout<<eigen_output<<std::endl;
-
-    };
-
-
-
 }}
 #endif
--- python/wavelets/wavelets.pyx
+++ python/wavelets/wavelets.pyx
@@ -5,14 +5,9 @@ from libcpp.string cimport string
 cdef extern from "python.wavelets.h" namespace "sopt::pyWavelets":
     void direct[T](T *signal, T* out, string name, 
                    unsigned long level, int nrow, int ncol);
-    void direct[T](T *signal, T *coefficient, T *out,
-                   string name, unsigned long level, int nrow,
-                   int ncol);
-    void indirect[T](T *signal, T *out, string name,\
-                     unsigned long level, int nrow, int ncol);
-    void indirect[T](T *signal, T *coefficient, T *out,
-                   string name, unsigned long level, int nrow,
-                   int ncol);
+    void indirect[T](T *signal, T* out, string name, 
+                   unsigned long level, int nrow, int ncol);
+
 
 def _getInDim(input):
     """ initialise output """
@@ -25,66 +20,30 @@ def _getInDim(input):
         input = input.reshape(nrow * ncol)
     return input, nrow, ncol
 
-def dwt(input, name, level, inverse = False, coefficient = None):
-    
+def dwt(input, name, level, inverse = False):
     input, nrow, ncol = _getInDim(input)
     output = np.zeros(nrow * ncol, dtype=input.dtype)
        
-    if coefficient is None:
-        coefficient = np.zeros(1, dtype=input.dtype)#dummy coefficient
-        WITH_COEFFICIENT = False 
-    else:
-        coefficient, nrow_cft, ncol_cft = _getInDim(coefficient)
-        if nrow_cft != nrow or ncol_cft != ncol:
-            raise ValueError('The dimension of coefficient should agree with that of input.\n')
-        WITH_COEFFICIENT = True
-        
     cdef:
         double[:] input_view = input
         double[:] output_view = output
-        double[:] coefficient_view = coefficient
         double *inptr = &input_view[0]
         double *outptr = &output_view[0]
-        double *cftptr = &coefficient_view[0]
         int c_nrow = nrow
         int c_ncol = ncol
         unsigned long c_level = level 
         string c_name = name
     
-    if inverse is False:
-        if WITH_COEFFICIENT:
-            direct(inptr, cftptr, outptr, c_name, c_level,
-                   c_nrow, c_ncol)
-        else:
-            direct(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
-    elif inverse is True:
-        if WITH_COEFFICIENT:
-            indirect(inptr, cftptr, outptr, c_name, c_level,
-                c_nrow, c_ncol)
-        else:
-            indirect(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
-    else:
-        raise ValueError('argument inverse should be True/False')
+    if inverse: direct(inptr, outptr, c_name, c_level,c_nrow, c_ncol)
+    else: indirect(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
     
     return output.reshape(nrow,ncol)
 
-
-
-def cdwt(input, name, level, inverse = False, coefficient = None):
-    """ initialise output """
-    assert input.ndim < 3
-    if input.ndim == 1:
-        nrow = input.size
-        ncol = 1
-    if input.ndim == 2:
-        nrow, ncol = input.shape
-        input = input.reshape(nrow * ncol)
-    WITH_COEFFICIENT = True
+def cdwt(input, name, level, inverse = False):
+    
+    input, nrow, ncol = _getInDim(input)
     output = np.zeros(nrow * ncol, dtype=input.dtype)
-    if coefficient is None:
-        coefficient = np.zeros(1, dtype=input.dtype)
-        WITH_COEFFICIENT = False 
-
+       
     cdef:
         double complex[:] input_view = input
         double complex[:] output_view = output
@@ -94,15 +53,8 @@ def cdwt(input, name, level, inverse = False, coefficient = None):
         int c_ncol = ncol
         unsigned long c_level = level 
         string c_name = name
-    if WITH_COEFFICIENT is True:
-        print 'with'
-    else:
-        if inverse is False:
-            direct(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
-        elif inverse is True:
-            indirect(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
-        else:
-            raise ValueError('argument inverse should be True/False')
+    
+    if inverse: indirect(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
+    else: direct(inptr, outptr, c_name, c_level, c_nrow, c_ncol)
+    
     return output.reshape(nrow,ncol)
-
-
