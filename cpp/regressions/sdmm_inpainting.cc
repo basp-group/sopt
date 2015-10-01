06588771622da48b588aedec447c535a04e60a47
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-01 12:10:08 +0100

    Adding sopt_l1_sdmm to regression
    Does not compile because not using Eigen::Ref, as I should

--- cpp/regressions/sdmm_inpainting.cc
+++ cpp/regressions/sdmm_inpainting.cc
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include <iostream>
 #include <iomanip>
+#include <type_traits>
 
 #include "sopt/sdmm.h"
 #include "sopt/logging.h"
@@ -11,6 +12,8 @@
 #include "sopt/proximal.h"
 #include "tools_for_tests/tiffwrappers.h"
 #include "tools_for_tests/directories.h"
+
+#include "sopt/sopt_l1.h"
 
 typedef double Scalar;
 typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> t_Vector;
@@ -50,21 +53,22 @@ t_Vector dirty(sopt::LinearTransform<t_Vector> const &sampling, sopt::t_rMatrix 
   return y;
 }
 
-sopt::algorithm::SDMM<Scalar> create_sdmm(
-    sopt::LinearTransform<t_Vector> const &sampling,
-    sopt::t_rMatrix const &image,
-    t_Vector const &y) {
-  using namespace sopt;
-
+Scalar epsilon(sopt::LinearTransform<t_Vector> const &sampling, sopt::t_rMatrix const &image) {
   auto const y0 = target(sampling, image);
   auto const nmeasure = y0.size();
-  auto epsilon = std::sqrt(nmeasure + 2*std::sqrt(nmeasure)) * sigma(sampling, y0);
-  auto prox_l2ball = proximal::translate(proximal::L2Ball<Scalar>(epsilon), -y);
+  return std::sqrt(nmeasure + 2*std::sqrt(nmeasure)) * sigma(sampling, y0);
+}
 
-  auto const wavelet = wavelets::factory("DB8", 4);
-  auto const psi = linear_transform<Scalar>(wavelet, image.rows(), image.cols());
 
-  auto relvar = RelativeVariation<Scalar>(1e-2);
+sopt::algorithm::SDMM<Scalar> create_sdmm(
+    sopt::LinearTransform<t_Vector> const &sampling,
+    sopt::LinearTransform<t_Vector> const &psi,
+    t_Vector const &y,
+    sopt_l1_sdmmparam const &params) {
+
+  using namespace sopt;
+  auto prox_l2ball = proximal::translate(proximal::L2Ball<Scalar>(params.epsilon), -y);
+  auto relvar = RelativeVariation<Scalar>(params.rel_obj);
   auto convergence = [&y, &sampling, &psi, &relvar](
       algorithm::SDMM<Scalar> const&, t_Vector const &x) {
     INFO("||x - y||_2: " << (y - sampling * x).stableNorm());
@@ -73,42 +77,87 @@ sopt::algorithm::SDMM<Scalar> create_sdmm(
     return relvar(x);
   };
 
-  // Now we can create the sdmm convex minimizer
-  // Its parameters are set by calling member functions with appropriate names.
   return algorithm::SDMM<Scalar>()
-    .itermax(500) // maximum number of iterations
-    .gamma(0.1)
-    .conjugate_gradient(300, 1e-8)
+    .itermax(params.max_iter)
+    .gamma(params.gamma)
+    .conjugate_gradient(params.cg_max_iter, params.cg_tol)
     .is_converged(convergence)
-    // Any number of (proximal g_i, L_i) pairs can be added
-    // ||Psi^dagger x||_1
     .append(proximal::l1_norm<Scalar>, psi.adjoint(), psi)
-    // ||y - A x|| < epsilon
     .append(prox_l2ball, sampling)
-    // x in positive quadrant
     .append(proximal::positive_quadrant<Scalar>);
+}
+
+// Wraps calls to sampling and wavelets to C style
+template<class T> struct CData {
+  typedef Eigen::Matrix<T, Eigen::Dynamic, 1> t_Vector;
+  typename t_Vector::Index nin, nout;
+  sopt::LinearTransform<t_Vector> const &transform;
+};
+
+template<class T> void direct_transform(void *out, void *in, void **data) {
+  CData<T> const &cdata = *(CData<T>*)data;
+  typedef Eigen::Matrix<T, Eigen::Dynamic, 1> t_Vector;
+  t_Vector input = t_Vector::Map((T*)in, cdata.nin);
+  t_Vector::Map((T*)out, cdata.nout) = cdata.transform * input;
+}
+template<class T> void adjoint_transform(void *out, void *in, void **data) {
+  CData<T> const &cdata = *(CData<T>*)data;
+  typedef Eigen::Matrix<T, Eigen::Dynamic, 1> t_Vector;
+  t_Vector input = t_Vector::Map((T*)in, cdata.nin);
+  t_Vector::Map((T*)out, cdata.nout) = cdata.transform.adjoint() * input;
 }
 
 TEST_CASE("Compare SDMMS", "") {
   using namespace sopt;
-
   // Read image and create target vector y
   t_rMatrix const image = notinstalled::read_standard_tiff(filename);
   t_uint const nmeasure = 0.5 * image.size();
   auto const sampling = linear_transform<Scalar>(Sampling(image.size(), nmeasure));
   auto const y = dirty(sampling, image);
 
+  sopt_l1_sdmmparam params = {
+    0,    // verbosity
+    50,   // max iter
+    0.1,  // gamma
+    0.01, // relative change
+    epsilon(sampling, image), // radius of the l2ball
+    1e-3, // Relative tolerance on epsilon
+    std::is_same<Scalar, sopt::real_type<Scalar>::type>::value ? 1: 0, // real data
+    200,  // cg max iter
+    1e-8, // cg tol
+  };
 
   // Create c++ SDMM
-  t_Vector result(image.size());
-  auto const sdmm = ::create_sdmm(sampling, image, y);
+  auto const wavelet = wavelets::factory("DB8", 4);
+  auto const psi = linear_transform<Scalar>(wavelet, image.rows(), image.cols());
+  auto const sdmm = ::create_sdmm(sampling, psi, y, params);
 
-  // Run sdmm
-  auto const diagnostic = sdmm(result, t_Vector::Zero(image.size()));
+  // Run sdmms
+  t_Vector cpp(image.size());
+  auto const diagnostic = sdmm(cpp, t_Vector::Zero(image.size()));
   CHECK(diagnostic.good);
+  t_Vector weights = t_Vector::Ones(image.size());
+  t_Vector c = t_Vector::Zero(image.size());
+  CData<Scalar> const sampling_data{image.size(), y.size(), sampling};
+  CData<Scalar> const psi_data{image.size(), y.size(), psi};
+  sopt_l1_sdmm(
+      (void*) c.data(), c.size(),
+      &direct_transform<Scalar>, (void**)&sampling_data,
+      &adjoint_transform<Scalar>, (void**)&sampling_data,
+      &adjoint_transform<Scalar>, (void**)&psi_data, // synthesis
+      &direct_transform<Scalar>, (void**)&psi_data,
+      c.size(),
+      (void*) y.data(), y.size(),
+      weights.data(),
+      params
+  );
 
   notinstalled::write_tiff(
-    t_Matrix::Map(result.data(), image.rows(), image.cols()),
-    outdir + "result_" + outfile
+    t_Matrix::Map(cpp.data(), image.rows(), image.cols()),
+    outdir + "cpp_" + outfile
+  );
+  notinstalled::write_tiff(
+    t_Matrix::Map(c.data(), image.rows(), image.cols()),
+    outdir + "c_" + outfile
   );
 }
