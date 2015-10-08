fcdc779eea2a3cf9b3ce772e98e94cfe33dc2173
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-08 19:26:32 +0100

    Regression oracle now current development branch

--- cmake_files/AddRegression.cmake
+++ cmake_files/AddRegression.cmake
@@ -6,7 +6,7 @@ include(catch)
 function(add_regression targetname)
   cmake_parse_arguments(regr "" "" "LIBRARIES;LABELS;DEPENDS;INCLUDES" ${ARGN})
   add_catch_test(${targetname} ${regr_UNPARSED_ARGUMENTS}
-    LIBRARIES ${Sopt_LIBRARIES} ${regr_LIBRARIES} 
+    LIBRARIES ${Sopt_LIBRARIES} ${regr_LIBRARIES}
     LABELS ${regr_LABELS} "regression"
     DEPENDS ${regr_DEPENDS} lookup_dependencies
     INCLUDES ${regr_INCLUDE} ${TIFF_INCLUDE_DIR} ${Sopt_INCLUDE_DIRS}
