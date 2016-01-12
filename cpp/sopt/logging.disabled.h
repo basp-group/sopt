c66809d443e108296217233d91613d19e757de21
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-12 15:27:01 +0000

    Function to check log level dynamically

--- cpp/sopt/logging.disabled.h
+++ cpp/sopt/logging.disabled.h
@@ -14,6 +14,7 @@ inline std::shared_ptr<int> get(std::string const &) { return nullptr; }
 inline std::shared_ptr<int> get() { return nullptr; }
 inline void set_level(std::string const &, std::string const &) {};
 inline void set_level(std::string const &) {};
+inline bool has_level(std::string const &, std::string const &) { return false; }
 }}
 
 //! \macro For internal use only
--- cpp/sopt/logging.enabled.h
+++ cpp/sopt/logging.enabled.h
@@ -52,6 +52,29 @@ namespace sopt { namespace logging {
       else
         SOPT_THROW("Unknown logging level ") << level << "\n";
   }
+
+  inline bool has_level(std::string const &level, std::string const &name="") {
+    auto const logger = get(name);
+    if(not logger)
+      return false;
+
+#   define SOPT_MACRO(LEVEL)                              \
+      if(level == #LEVEL)                                 \
+          return logger->level() >= spdlog::level::LEVEL
+      SOPT_MACRO(trace);
+      else SOPT_MACRO(debug);
+      else SOPT_MACRO(info);
+      else SOPT_MACRO(notice);
+      else SOPT_MACRO(warn);
+      else SOPT_MACRO(err);
+      else SOPT_MACRO(critical);
+      else SOPT_MACRO(alert);
+      else SOPT_MACRO(emerg);
+      else SOPT_MACRO(off);
+#   undef SOPT_MACRO
+      else
+        SOPT_THROW("Unknown logging level ") << level << "\n";
+  }
 }}
 
 //! \macro For internal use only
