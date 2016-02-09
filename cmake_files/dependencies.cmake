a758c06813cae432c01be5c9e15aa1c7b0716721
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-02-09 17:28:48 +0000

    Can run regression against C version in same worktree

--- cmake_files/dependencies.cmake
+++ cmake_files/dependencies.cmake
@@ -8,8 +8,12 @@ endif()
 if(examples OR regression OR Csopt)
   find_package(TIFF REQUIRED)
 endif()
-if(regressions AND NOT Csopt)
+
+if(regressions OR Csopt)
   find_package(FFTW3 REQUIRED DOUBLE)
+endif()
+
+if(regressions AND NOT Csopt)
   lookup_package(Sopt
     REQUIRED DOWNLOAD_BY_DEFAULT
     PATHS "${EXTERNAL_ROOT}"
@@ -19,10 +23,12 @@ if(regressions AND NOT Csopt)
       GIT_REPOSITORY "git@github.com:astro-informatics/sopt"
       GIT_TAG ${REGRESSION_ORACLE_ID}
   )
+elseif(regressions AND Csopt)
+  set(Sopt_LIBRARIES libsopt)
+  set(Sopt_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/include")
 endif()
 
 if(Csopt)
-  find_package(FFTW3 COMPONENTS DOUBLE)
   find_package(CBLAS REQUIRED)
   # On some (linux) machines we also need libm to compile sopt_demo*.c
   # Make a half-hearted attempt at finding it.
