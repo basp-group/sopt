4e1837f61fa79da4eaca4190459ae937ccdbd15c
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-02-18 12:17:33 +0000

    Make some functions extern inline

--- include/sopt_error.h
+++ include/sopt_error.h
@@ -13,20 +13,8 @@
 extern "C" {
 #endif
 
-inline void SOPT_ERROR_GENERIC(char *comment) {
-  printf("ERROR: %s.\n", comment);					
-  printf("ERROR: %s <%s> %s %s %s %d.\n",				
-    "Occurred in function",					
-    __PRETTY_FUNCTION__,						
-    "of file", __FILE__,						
-    "on line", __LINE__);					
-  exit(1);
-}
-
-inline void SOPT_ERROR_MEM_ALLOC_CHECK(void *pointer) {				
-  if(pointer == NULL)
-    SOPT_ERROR_GENERIC("Memory allocation failed");
-}
+extern inline void SOPT_ERROR_GENERIC(char *comment);
+extern inline void SOPT_ERROR_MEM_ALLOC_CHECK(void *pointer);
 
 #ifdef __cplusplus
 }
